1e55a59609ba051a6836365866be31d15dd8efab
Mahdi Jahed <mahdijahed56@gmail.com>
2025-12-15 18:45:36 +0330

    Clean up code and structure

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,9 @@ import Footer from './components/Footer';
 
 export default function App() {
 
-  const [theme, setTheme] = useState<'dark' | 'light'>('dark')
-
-  useEffect(() => {
-    // apply class on root element for light/dark css tweaks (we used :root.light in index.css)
-    if (theme === 'light') {
-      document.documentElement.classList.add('light')
-    } else {
-      document.documentElement.classList.remove('light')
-    }
-  }, [theme])
-
   return (
-    <div className={`min-h-screen bg-gradient-to-br  ${theme === 'dark' ? "from-gray-950 to-blue-950 text-white" : "from-slate-50 to-slate-100 text-black"}`}>
-      <Navbar theme={theme} setTheme={setTheme} />
+    <div className={`min-h-screen bg-gradient-to-br from-gray-950 to-blue-950 text-white}`}>
+      <Navbar />
       <main>
         <Home />
         <About />
