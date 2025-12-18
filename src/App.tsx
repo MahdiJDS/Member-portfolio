24f1849a36f1a1231a4fe91771d261e7763743aa
Mahdi Jahed <mahdijahed56@gmail.com>
2025-12-18 12:03:04 +0330

    feat: Use lazy load for optimization App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import Home from './Page/Home';
-import About from './Page/About';
-import Skills from './Page/Skills';
+// import Home from './Page/Home';
+// import About from './Page/About';
+// import Skills from './Page/Skills';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import ContactUs from './Page/Contact';
+// import ContactUs from './Page/Contact';
+import { lazy, Suspense } from 'react';
+import Loader from './components/Loader';
+
+const Home = lazy(() => import('./Page/Home'));
+const About = lazy(() => import('./Page/About'));
+const Skills = lazy(() => import('./Page/Skills'));
+const ContactUs = lazy(() => import('./Page/Contact'));
 
 export default function App() {
 
@@ -11,12 +18,20 @@ export default function App() {
     <div className={`min-h-screen bg-gradient-to-br from-gray-950 to-blue-950 text-white}`}>
       <Navbar />
       <main>
-        <Home />
-        <About />
-        <Skills />
-        <ContactUs />
+        <Suspense fallback={<Loader />} >
+          <Home />
+        </Suspense>
+        <Suspense fallback={<Loader />} >
+          <About />
+        </Suspense>
+        <Suspense fallback={<Loader />} >
+          <Skills />
+        </Suspense>
+        <Suspense fallback={<Loader />} >
+          <ContactUs />
+        </Suspense>
         <Footer />
       </main>
-    </div>
+    </div >
   );
 }
